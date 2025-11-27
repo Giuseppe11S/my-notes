d866d2ef8b3bbd8231c65e91d7c778451c843b33
Giuseppe11S <senagiuseppe11@gmail.com>
2025-11-27 09:21:23 +0100

    Implemented random background color for my notes

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,23 +1,31 @@
+import { useState } from 'react'
+import { useEffect } from 'react';
 import Noteitem from '../components/NoteItem'
 
 import exampleData from '../data/exampleNotes'
 
 export default function NoteList({newNote, onDeleteNotes}) {
+  
+  // array of colors that im using in the background color of notes
+  const randomColor = ['#D1FAE5', '#FEF3C7','#FCE7F3','#DBEAFE','#F3E8FF']
 
   // rendering notes items
   const renderingNote = () => (
-    newNote.map((note) => (
-      <div key={note.id} 
-         className='bg-[#c6ecd2] rounded-lg w-[300px] h-[200px] py-[15px] px-[20px] relative
-         overflow-y-scroll [scrollbar-width:none] -ms-overflow-style:none] [&::-webkit-scrollbar]:hidden'>
-        <Noteitem 
-        title={note.title}
-        text={note.text}
-        id={note.id}
-        onDeleteNotes={onDeleteNotes}
-        />
+    newNote.map((note) => {
+      // it generates random color
+      const selectColor = randomColor[Math.floor(Math.random()*randomColor.length)]
+      return <div key={note.id} 
+         style={{ backgroundColor: `${selectColor}` }}
+         className={`rounded-lg w-[300px] h-[200px] py-[15px] px-[20px] relative
+         overflow-y-scroll [scrollbar-width:none] -ms-overflow-style:none] [&::-webkit-scrollbar]:hidden `}>
+          <Noteitem 
+          title={note.title}
+          text={note.text}
+          id={note.id}
+          onDeleteNotes={onDeleteNotes}
+          />
       </div>
-    ))
+      })
   )
 
   return (
